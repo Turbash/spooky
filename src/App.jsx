640858ae245b6c83f630d1a6e4124155b0854582
Turbash <negirawatdeepi@gmail.com>
2025-10-29 10:59:56 +0530

    About section and 3d model added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import Navbar from './components/Navbar.jsx'
 import Hero from './sections/Hero.jsx'
+import { Pumpkin } from './components/Pumpkin.jsx'
+import { Canvas } from '@react-three/fiber'
+import {Center, OrbitControls} from '@react-three/drei'
+import About from './sections/About.jsx'
 
 const App = () => {
   return (
     <>
+      <figure className='absolute top-[11vh] z-20' style={{ width: '100vw', height: '100vh' }}>
+        <Canvas shadows camera={{ position: [10, 20, 0], fov: 30 }}>
+          <ambientLight intensity={1} />
+          <directionalLight position={[3, 3, 3]} intensity={1.5} />
+          <Center>
+            <Pumpkin />
+          </Center>
+          <OrbitControls enableZoom={false} />
+        </Canvas>
+      </figure>
       <Navbar />
       <Hero />
+      <About />
     </>
   )
 }
---
+++ src/sections/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import bgImg from '../assets/halloween-bg.jpg'
+
+const About = () => {
+  return (
+    <div className='relative'>
+        <img src={bgImg} alt="" />    
+        <div className='absolute bottom-1/3 right-1/10 max-w-2xl text-2xl leading-10 bg-[#00000070] px-8 py-6 rounded-4xl'>
+            This project shows how to create a 3D model using React + GSAP + Three.js.    
+        </div>    
+    </div>
+  )
+}
+
+export default About
