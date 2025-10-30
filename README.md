876944be3baf75dbb8176e6da43c07354e5658d7
Turbash <negirawatdeepi@gmail.com>
2025-10-30 17:35:10 +0530

    attempt to make it responsive

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
-# React + Vite
+# Spooky Website
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## A Halloween themed website 
 
-Currently, two official plugins are available:
+A spooky halloween themed website, made with three js, react fibre and gsap libraries to give a nice illustration of the power of three js and gsap combined.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) (or [oxc](https://oxc.rs) when used in [rolldown-vite](https://vite.dev/guide/rolldown)) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Requirements
 
-## React Compiler
+- A decent version of node and npm.
 
-The React Compiler is not enabled on this template because of its impact on dev & build performances. To add it, see [this documentation](https://react.dev/learn/react-compiler/installation).
+## How to run locally
 
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import { useRef } from "react";
 
 const Navbar = () => {
+  const overlayRef = useRef(null);
+
+  const closeOverlay = () => {
+    if (overlayRef.current) {
+      overlayRef.current.style.display = "none";
+    }
+  };
   return (
     <div className="w-full absolute inset-0 z-10">
-      <div className="container flex justify-between items-center py-7">
-        <a href="" className="font-semibold text-2xl">
+      <div className="container flex justify-between items-center py-7 2xl:py-10">
+        <a href="" className="font-semibold text-lg lg:text-2xl 2xl:text-3xl">
           Rupz Web
         </a>
-        <ul className="flex items-center gap-12">
+        <ul className="hidden sm:flex items-center gap-12 text-sm lg:text-xl 2xl:text-2xl">
           <li>
-            <a href="">About</a>
+            <a href="#about">About</a>
           </li>
           <li>
             <a href="">Demo</a>
@@ -23,6 +30,25 @@ const Navbar = () => {
             </a>
           </li>
         </ul>
+        <div className="sm:hidden flex bg-black absolute inset-0 z-[150] flex-col gap-1">
+          <span className="text-orange absolute right-5 top-3" onClick={closeOverlay}>x</span>
+          <ul className="flex mt-10 flex-col items-center gap-12 text-sm lg:text-xl 2xl:text-2xl">
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="">Demo</a>
+            </li>
+            <li>
+              <a
+                href=""
+                className="text-orange px-5 py-2 border-2 bg-transparent border-orange rounded-full font-semibold hover:bg-orange hover:text-white transition"
+              >
+                Code Repo
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
